eedc676ea52b0cedb51308d8b451505703d64cfb
kikikanik <55113869+kikikanik@users.noreply.github.com>
2020-03-05 20:22:30 -0500

    version2

--- src/numberGuessingGameCode.java
+++ src/numberGuessingGameCode.java
@@ -5,20 +5,27 @@ public class numberGuessingGameCode {
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
 		Random rand = new Random();
-		
-		int randomNumber = rand.nextInt(5);
-	//	int randomNumber2 = 1 + rand.nextInt(5);
-		
-		System.out.println ("Welcome to the Number Guessing Game!");
-		System.out.print("We will give you one try to guess what our secret number is. Enter your guess Now:");
-		int guess = in.nextInt();
-		if (guess == randomNumber) {
-			System.out.print("Our secret has been found out! Congradulations");
-		}
-		else {
-			System.out.print("Incorrect, my friend. It is: " + randomNumber);
-		}
-		in.close();
-	}
 
+		int randomNumber = rand.nextInt(5);
+		int guess;
+		//	int guessCount 
+		//	int randomNumber2 = 1 + rand.nextInt(5);
+
+		System.out.println ("Welcome to the Number Guessing Game! Try to find out our secret. Our secret number is between 0 and 5");
+
+		System.out.print("Enter your guess Now:");
+		guess = in.nextInt();
+		while(true) {
+			if (guess == randomNumber) {
+				break;
+			}
+			while (guess != randomNumber) {
+				System.out.print("Incorrect, my friend. Try again:");
+				guess = in.nextInt();
+			}
+			System.out.print("Our secret has been found out! Congradulations");
+			in.close();
+		}
+
+	}
 }
